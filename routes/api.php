f96145b26a72e55f2e0bb052372406d6d2521b6c
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-06-27 21:15:32 +0700

    Add: Api routes

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::prefix('v1')->group(function () {
+    Route::get('/', function (Request $request) {
+        return 'Running';
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me'])->middleware('CheckAuth');
+        Route::get('/verify/{token}', [AuthController::class, 'verifyEmail']);
+    });
+});
