8989fb598dee0e7c1028c8d0cd4423f9ce16290f
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-06-27 21:15:25 +0700

    Update: User migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,20 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('instagram')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['ADMIN', 'USER'])->default('USER');
+            $table->boolean('is_verified')->default(false);
+            $table->string('verification_token')->nullable();
+            $table->string('reset_token')->nullable();
+            $table->timestamp('reset_expires')->nullable();
+            $table->string('public_id')->nullable();
+            $table->string('image_url')->nullable();
             $table->timestamps();
         });
 
