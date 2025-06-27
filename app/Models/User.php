61b0bf961a9f3797baaeb8f5c57a504ae5dbbc64
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-06-27 21:13:56 +0700

    Update: User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -17,21 +19,24 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(fn($model) => ($model->id = Str::uuid()));
+    }
+
+    protected $fillable = ['name', 'email', 'phone', 'instagram', 'password', 'role', 'is_verified', 'verification_token', 'reset_token', 'reset_expires', 'public_id', 'image_url'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var list<string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
@@ -44,5 +49,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function articlesEdited(): HasMany
+    {
+        return $this->hasMany(Article::class, 'editor_id');
     }
 }
