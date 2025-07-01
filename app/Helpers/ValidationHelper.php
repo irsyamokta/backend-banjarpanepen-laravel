67d1aa57218a36ebca95ba34609e9235aedfb8cd
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-07-01 13:51:04 +0700

    Update: User validation

--- app/Helpers/ValidationHelper.php
+++ app/Helpers/ValidationHelper.php
@@ -46,4 +46,26 @@ class ValidationHelper
             'password.required' => 'Password tidak boleh kosong!',
         ]);
     }
+
+    public static function updateUser($data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|min:3|max:50',
+            'phone' => 'required|string|min:10|max:15|regex:/^[0-9]+$/',
+            'email' => 'required|email',
+            'instagram' => 'required|string|min:3|max:50',
+        ], [
+            'name.min' => 'Nama minimal harus 3 karakter!',
+            'name.max' => 'Nama maksimal 50 karakter!',
+            'name.required' => 'Nama tidak boleh kosong!',
+            'phone.required' => 'Nomor telepon tidak boleh kosong!',
+            'phone.min' => 'Nomor telepon minimal harus 10 karakter!',
+            'phone.max' => 'Nomor telepon maksimal 15 karakter!',
+            'email.email' => 'Format email tidak valid!',
+            'email.required' => 'Email tidak boleh kosong!',
+            'instagram.required' => 'Instagram tidak boleh kosong!',
+            'instagram.min' => 'Instagram minimal harus 3 karakter!',
+            'instagram.max' => 'Instagram maksimal 50 karakter!',
+        ]);
+    }
 }
